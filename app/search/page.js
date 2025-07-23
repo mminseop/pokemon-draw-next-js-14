22235042b99077090affb50a4d8b8ae262be89d6
mminseop <alstjq903@naver.com>
2025-07-23 22:58:29 +0900

    search Suspense 추가 (vercel nex.js 14 배포 오류 수정)

--- app/search/page.js
+++ app/search/page.js
@@ -1,61 +1,18 @@
-"use client";
-
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useSearchParams, useRouter } from "next/navigation";
-import { fetchPokemonList } from "@/app/RTK/pokemonSlice";
-import PokemonCard from "@/app/components/PokemonCard";
+import React, { Suspense } from "react";
+import Search from "./search";
 import { ClipLoader } from "react-spinners";
-import { getRegExp } from "korean-regexp";
 
-function Search() {
-  const params = useSearchParams();
-  const query = params.get("pokemon") || "";
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const { list: pokemonList, loading } = useSelector((state) => state.pokemon);
-
-  useEffect(() => {
-    if (query === "") {
-      router.push("/");
-    }
-  }, [query, router]);
-
-  useEffect(() => {
-    if (pokemonList.length === 0) {
-      dispatch(fetchPokemonList(151));
-    }
-  }, [dispatch, pokemonList.length]);
-
-  const searchPokemonList = pokemonList.filter((item) =>
-    getRegExp(query).test(item.koreaName)
-  );
-
-  if (loading)
-    return (
-      <>
+export default function SearchPage() {
+  return (
+    <Suspense fallback={
+        <>
         <div className="flex justify-center items-center h-30">
           <ClipLoader color="#333" size={30} />
         </div>
         <div className="text-gray-700 text-2xl">포켓몬을 불러오는 중...</div>
       </>
-    );
-
-  if (searchPokemonList.length === 0) {
-    return (
-      <div className="text-gray-700 text-3xl m-[20%]">포켓몬이 없습니다.</div>
-    );
-  }
-
-  return (
-    <section className="flex justify-center px-6 py-8 bg-gray-50">
-      <div className="flex flex-wrap gap-6 justify-start max-w-[1100px]">
-        {searchPokemonList.map((pokemon) => (
-          <PokemonCard key={pokemon.id} pokemon={pokemon} />
-        ))}
-      </div>
-    </section>
+    }>
+      <Search />
+    </Suspense>
   );
 }
-
-export default Search;
