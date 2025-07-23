bf71295a8386e4b2169dea428f3f8a6247fb87f8
mminseop <alstjq903@naver.com>
2025-07-23 23:42:58 +0900

    Google Search Console 소유권 meta태그 접두어 추가

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,10 @@ import { Providers } from "./RTK/provider";
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <head>
+         <title>포켓몬 도감</title>
+        <meta name="google-site-verification" content="7Jyxop61CiR39eKHfRyGHigXt0xcrMnx7Mk9Pm8VJ_c" />
+      </head>
       <body>
         <Providers>
           <Header />
